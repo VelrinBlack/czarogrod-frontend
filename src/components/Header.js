39af8435aebe60f5c890c9ea5691d53597e13209
velrinblack <velrinblack@gmail.com~>
2020-08-22 19:07:37 +0200

    Task 2a: Made hamburger sticky

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,28 @@
 // react
 import React from 'react';
 
+// external libraries
+import styled from 'styled-components';
+
 // components
 import TopBar from './TopBar';
 import Navbar from './Navbar/Navbar';
 
-const Header = () => {
-    return (
-        <header>
-            <TopBar />
-            <Navbar />
-        </header>
-    );
-};
+
+const StyledHeader = styled.header`
+    position: sticky;
+    top: -20%;
+
+    height: 290px;
+
+    background-color: #fff;
+`
+
+const Header = () => (
+    <StyledHeader>
+        <TopBar />
+        <Navbar />
+    </StyledHeader>
+);
 
 export default Header;
