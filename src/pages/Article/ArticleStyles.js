ada6eac58b22c51b55d76942b86d9002812c3ebe
velrinblack <velrinblack@gmail.com>
2020-11-16 14:50:16 +0100

    Fix font size in blog articles

--- src/pages/Article/ArticleStyles.js
+++ src/pages/Article/ArticleStyles.js
@@ -34,28 +34,20 @@ export const StyledContainer = styled.div`
   .txt {
     width: 50%;
     margin: 0 auto;
+    font-size: 20px;
 
     @media (max-width: 1920px) {
       width: 60%;
+      font-size: 18px;
     }
 
     @media (max-width: 1024px) {
       width: 70%;
+      font-size: 16px;
     }
 
     @media (max-width: 786px) {
       width: 90%;
-    }
-
-    p {
-      font-size: 20px;
-
-      @media (max-width: 1920px) {
-        font-size: 16px;
-      }
-      @media (max-width: 1280px) {
-        font-size: 15px;
-      }
     }
 
     img {
