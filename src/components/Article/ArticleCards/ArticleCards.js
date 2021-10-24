1b5ca06f59f11d6f2a6157587ac7a34e12713dea
Velrin Black <velrinblack@gmail.com>
2021-10-24 16:05:02 +0200

    Fix article cards error

--- src/components/Article/ArticleCards/ArticleCards.js
+++ src/components/Article/ArticleCards/ArticleCards.js
@@ -1,20 +1,20 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import router from 'next/router';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import router from "next/router";
 
-import { StyledContainer } from './ArticleCardsStyles';
+import { StyledContainer } from "./ArticleCardsStyles";
 
 const Article = ({ currentPost }) => {
   const [articles, setArticles] = useState([]);
 
   useEffect(() => {
     axios
-      .get('https://czarogrod-backend-strapi.herokuapp.com/posts')
+      .get("https://czarogrod-backend-strapi.herokuapp.com/posts")
       .then((res) => {
         res.data = res.data.filter((article) => article.id !== currentPost.id);
         res.data = res.data.reverse();
 
-        if (res.data.length > 5) res.data.splice(0, 5);
+        if (res.data.length > 5) res.data = res.data.splice(0, 5);
 
         setArticles(res.data);
       });
@@ -23,22 +23,22 @@ const Article = ({ currentPost }) => {
   return (
     <>
       <StyledContainer>
-        <div className='cards-wrapper'>
+        <div className="cards-wrapper">
           {articles.map((article) => (
             <a
-              className='card'
+              className="card"
               href={`/blog/${article.id}`}
               onClick={(e) => {
                 e.preventDefault();
                 router.push(`/blog/${article.id}`);
                 window.scroll({
                   top: 0,
-                  behavior: 'smooth',
+                  behavior: "smooth",
                 });
               }}
             >
               <h2>{article.title}</h2>
-              <img src={article.image.url} alt='zdjęcie główne' />
+              <img src={article.image.url} alt="zdjęcie główne" />
             </a>
           ))}
         </div>
